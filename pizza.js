f25166a5ca31991efe506e867bd13aa26e6cc706
gitmesomepizza <41208716+gitmesomepizza@users.noreply.github.com>
2018-07-13 16:21:57 -0400

    Update pizza.js

--- pizza.js
+++ pizza.js
@@ -1,5 +1,5 @@
 window.onload = function(){
-        document.getElementById("toolbar-pause-all").insertAdjacentHTML('afterend', '<div id="toolbar-separator"></div><div id="toolbar-pizza" title="Forward Port"></div><div id="toolbar-pizza2" title="Port" style="display:none;">0</div>');
+        document.getElementById("toolbar-pause-all").insertAdjacentHTML('afterend', '<div id="toolbar-separator"></div><div id="toolbar-pizza" title="Click me!"></div><div id="toolbar-pizza2" title="Port" style="display:none;">0</div>');
         document.getElementById("toolbar-pizza").style.backgroundImage = "url('style/transmission/images/toolbar-pizza.png')";
         $('#toolbar-pizza').click(function(){$.get("https://www.domain.com/pizza.php", function(result){ $('#toolbar-pizza2').text(result).show(); });});
 }
